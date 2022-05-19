11fb93587fbeef77124a026b3b970abd27a517f4
SadhanaAnandan <102043923+SadhanaAnandan@users.noreply.github.com>
2022-05-19 22:38:22 +0200

    The theme 3 is Big Hero 6. Home page button for big hero 6 shows baymax character and its loading screen shows two of the main characters on it. Theme 3 for homepage/loading screen is done.

--- LittleDrivers/app/src/main/java/com/example/littledrivers/Loading3Activity.java
+++ LittleDrivers/app/src/main/java/com/example/littledrivers/Loading3Activity.java
@@ -33,7 +33,7 @@ public class Loading3Activity extends AppCompatActivity {
         progressAnimation();
 
         Drawable progressDrawable = progressBar.getProgressDrawable().mutate();
-        progressDrawable.setColorFilter(Color.LTGRAY, android.graphics.PorterDuff.Mode.SRC_IN);
+        progressDrawable.setColorFilter(Color.BLACK, android.graphics.PorterDuff.Mode.SRC_IN);
         progressBar.setProgressDrawable(progressDrawable);
     }
 
