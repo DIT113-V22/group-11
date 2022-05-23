de32b6a5d7d645fef0e37729c26d388a73044cd5
AnashaSarker2001 <102026572+AnashaSarker2001@users.noreply.github.com>
2022-05-23 15:58:02 +0200

    Intentionally breaking the CI to see if the CI catches the error

--- LittleDrivers/app/src/androidTest/java/com/example/littledrivers/MainActivityTest.kt
+++ LittleDrivers/app/src/androidTest/java/com/example/littledrivers/MainActivityTest.kt
@@ -4,8 +4,11 @@ import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.example.littledrivers.MainActivity
 import org.junit.Test
@@ -14,8 +17,69 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
     @Test
-    fun test_isActivity_inView() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.main)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    fun test_navHomePage_theme1() {
+        val activityScenario = ActivityScenario.launch(HomeActivity::class.java)
+
+        onView(withId(R.id.Theme1)).perform(click())
+        onView(withId(R.id.Theme1Activity)).check(matches(isDisplayed()))
     }
+
+    @Test
+    fun theme1_back_to_HomePage() {
+        val activityScenario = ActivityScenario.launch(Theme1Activity::class.java)
+
+        onView(withId(R.id.home1)).perform(click())
+        onView(withId(R.id.homeActivity)).check((matches(isDisplayed())))
+    }
+
+    @Test
+    fun test_navHomePage_theme2() {
+        val activityScenario = ActivityScenario.launch(HomeActivity::class.java)
+
+        onView(withId(R.id.Theme2)).perform(click())
+        onView(withId(R.id.Theme2Activity)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun theme2_back_to_HomePage() {
+        val activityScenario = ActivityScenario.launch(Theme2Activity::class.java)
+
+        onView(withId(R.id.home2)).perform(click())
+        onView(withId(R.id.homeActivity)).check((matches(isDisplayed())))
+    }
+
+    @Test
+    fun test_navHomePage_theme3() {
+        val activityScenario = ActivityScenario.launch(HomeActivity::class.java)
+
+        onView(withId(R.id.Theme3)).perform(click())
+        onView(withId(R.id.Theme3Activity)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun theme3_back_to_HomePage() {
+        val activityScenario = ActivityScenario.launch(Theme3Activity::class.java)
+
+        onView(withId(R.id.home3)).perform(click())
+        onView(withId(R.id.homeActivity)).check((matches(isDisplayed())))
+    }
+
+    @Test
+    fun test_navHomePage_theme4() {
+        val activityScenario = ActivityScenario.launch(HomeActivity::class.java)
+
+        onView(withId(R.id.Theme4)).perform(click())
+        onView(withId(R.id.Theme4Activity)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun theme4_back_to_HomePage() {
+        val activityScenario = ActivityScenario.launch(Theme4Activity::class.java)
+
+        onView(withId(R.id.home4)).perform(click())
+        onView(withId(R.id.homeActivity)).check((matches(isDisplayed())))
+    }
+
+
+
 }
