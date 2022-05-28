2bec9874a62411edf9931696b2150edab278a303
SadhanaAnandan <102043923+SadhanaAnandan@users.noreply.github.com>
2022-05-28 14:41:48 +0200

    The main activity java and xml file were deleted as they are no longer used in this project.

--- LittleDrivers/app/src/androidTest/java/com/example/littledrivers/NavigationTest.kt
+++ LittleDrivers/app/src/androidTest/java/com/example/littledrivers/NavigationTest.kt
@@ -2,16 +2,11 @@ package com.example.littledrivers
 
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.scrollTo
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import com.example.littledrivers.MainActivity
 import org.junit.Test
 import org.junit.runner.RunWith
 
